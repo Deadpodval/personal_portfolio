8889d2b4b82f74b368433a922a3f14e2ad5e788a
Deadpodval <timofei_kahharov@mail.ru>
2023-01-12 14:27:49 +0600

    last commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    content = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import User, Post
 
-# Create your views here.
+
+def all_blogs(request):
+    users = User.objects.all()
+    return render(request, 'blog/all_blogs.html', {'users': users})
+
+
+def blog(request, blog_id: int):
+    post = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'blog/detail.html', {'post': post})
+
+
+def user_posts(request, user):
+    user = User.objects.get(name=user)
+    posts = Post.objects.order_by('-date')
+    return render(request, 'blog/user_posts.html', {'user': user, 'posts': posts})
+
+
